610a673fb59915c8bebbc49150ed863e5f2b74dd
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-01-28 17:13:59 -0600

    Update Plugin.cs

--- GorillaGoSmall SC/Plugin.cs
+++ GorillaGoSmall SC/Plugin.cs
@@ -2,6 +2,7 @@ using BepInEx;
 using System;
 using System.ComponentModel;
 using UnityEngine;
+using GorillaLocomotion;
 using Utilla;
 
 namespace GorillaGoSmallGorillaGoBig
@@ -19,8 +20,6 @@ namespace GorillaGoSmallGorillaGoBig
     {
         public bool IsEnabled;
         public bool inRoom = false;
-        public GameObject SizeChanger;
-        public Transform Parent;
 
         void Start()
         {
@@ -28,6 +27,20 @@ namespace GorillaGoSmallGorillaGoBig
 			/* Put code in OnGameInitialized to avoid null references */
 
             Utilla.Events.GameInitialized += OnGameInitialized;
+            SetScale(1, false);
+        }
+
+        public void SetScale(float scale, bool isSmall) //Code made by dev and is from my other mod "Gorilla's Doom" Thanks dev for helping!
+        {
+            Player.Instance.TryGetComponent(out SizeManager sizeManager);
+            if (isSmall)
+            {
+                sizeManager.enabled = false;
+                Player.Instance.scale = scale;
+                return;
+            }
+
+            sizeManager.enabled = true;
         }
 
         void OnEnable()
@@ -37,6 +50,15 @@ namespace GorillaGoSmallGorillaGoBig
 
             HarmonyPatches.ApplyHarmonyPatches();
             IsEnabled = true;
+
+            if(inRoom == true)
+            {
+                SetScale(0.1f, true);
+            }
+            else
+            {
+                SetScale(1, false);
+            }
         }
 
         void OnDisable()
@@ -46,41 +68,19 @@ namespace GorillaGoSmallGorillaGoBig
             /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
 
             HarmonyPatches.RemoveHarmonyPatches();
-            SizeChanger.transform.localScale = new Vector3(0.3422f, 0.3422f, 0.3422f);
             IsEnabled = false;
-            SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
+            SetScale(1, false);
         }
 
         void OnGameInitialized(object sender, EventArgs e)
         {
+            SetScale(1, false);
             /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-
-            GameObject.CreatePrimitive(PrimitiveType.Cube).name = "SizeChangerModdedLobby";
-            Parent = transform.Find("SizeChangerModdedLobby");
-            SizeChanger = GameObject.Find("SnowGlobeTriggerVolume");
-
-            SizeChanger.transform.parent = Parent;
-            SizeChanger.transform.localScale = new Vector3(0.3422f, 0.3422f, 0.3422f);
         }
 
         void Update()
         {
             /* Code here runs every frame when the mod is enabled */
-
-            if (IsEnabled == false)
-            {
-                SizeChanger.transform.localScale = new Vector3(0.3422f, 0.3422f, 0.3422f);
-            }
-
-            if (inRoom == true)
-            {
-                SizeChanger.transform.localScale = new Vector3(999, 999, 999);
-            }
-
-            if (inRoom == false)
-            {
-                SizeChanger.transform.localScale = new Vector3(0.3422f, 0.3422f, 0.3422f);
-            }
         }
 
         /* This attribute tells Utilla to call this method when a modded room is joined */
@@ -91,7 +91,7 @@ namespace GorillaGoSmallGorillaGoBig
             /* This code will run regardless of if the mod is enabled*/
 
             inRoom = true;
-            SizeChanger.GetComponent<SizeChanger>().minScale = 0.1f;
+            SetScale(0.1f, true);
         }
 
         /* This attribute tells Utilla to call this method when a modded room is left */
@@ -100,10 +100,8 @@ namespace GorillaGoSmallGorillaGoBig
         {
             /* Deactivate your mod here */
             /* This code will run regardless of if the mod is enabled*/
-
-            SizeChanger.transform.localScale = new Vector3(0.3422f, 0.3422f, 0.3422f);
             inRoom = false;
-            SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
+            SetScale(1, false);
         }
     }
 }
