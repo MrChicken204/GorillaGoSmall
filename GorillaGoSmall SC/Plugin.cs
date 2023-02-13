72ff4d0c5ef1e180064903ba209234f9b8941dc6
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-02-13 17:30:56 -0600

    Update Plugin.cs

--- GorillaGoSmall SC/Plugin.cs
+++ GorillaGoSmall SC/Plugin.cs
@@ -2,8 +2,12 @@ using BepInEx;
 using System;
 using System.ComponentModel;
 using UnityEngine;
-using GorillaLocomotion;
 using Utilla;
+using Player = GorillaLocomotion.Player;
+using BepInEx.Configuration;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace GorillaGoSmallGorillaGoBig
 {
@@ -18,8 +22,11 @@ namespace GorillaGoSmallGorillaGoBig
     [Description("HauntedModMenu")]
     public class Plugin : BaseUnityPlugin
     {
+        public static Plugin Instance { get; private set; }
         public bool IsEnabled;
         public bool inRoom = false;
+        public static ConfigEntry<float> Size;
+        public GameObject ScaleChanger;
 
         void Start()
         {
@@ -27,37 +34,26 @@ namespace GorillaGoSmallGorillaGoBig
 			/* Put code in OnGameInitialized to avoid null references */
 
             Utilla.Events.GameInitialized += OnGameInitialized;
-            SetScale(1, false);
-        }
-
-        public void SetScale(float scale, bool isSmall) //Code made by dev and is from my other mod "Gorilla's Doom" Thanks dev for helping!
-        {
-            Player.Instance.TryGetComponent(out SizeManager sizeManager);
-            if (isSmall)
-            {
-                sizeManager.enabled = false;
-                Player.Instance.scale = scale;
-                return;
-            }
-
-            sizeManager.enabled = true;
+            ScaleChanger.SetActive(false);
+            ScaleChanger.GetComponent<SizeChanger>().minScale = 1f;
         }
 
         void OnEnable()
         {
             /* Set up your mod here */
             /* Code here runs at the start and whenever your mod is enabled*/
-
             HarmonyPatches.ApplyHarmonyPatches();
             IsEnabled = true;
 
             if(inRoom == true)
             {
-                SetScale(0.1f, true);
+                ScaleChanger.SetActive(true);
+                ScaleChanger.GetComponent<SizeChanger>().minScale = Size.Value;
             }
             else
             {
-                SetScale(1, false);
+                ScaleChanger.SetActive(false);
+                ScaleChanger.GetComponent<SizeChanger>().minScale = 1f;
             }
         }
 
@@ -69,18 +65,24 @@ namespace GorillaGoSmallGorillaGoBig
 
             HarmonyPatches.RemoveHarmonyPatches();
             IsEnabled = false;
-            SetScale(1, false);
         }
 
         void OnGameInitialized(object sender, EventArgs e)
         {
-            SetScale(1, false);
-            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-        }
-
-        void Update()
-        {
-            /* Code here runs every frame when the mod is enabled */
+            //This is for one object...
+            new GameObject().name = "ScaleChanger";
+            ScaleChanger = GameObject.Find("ScaleChanger");
+            ScaleChanger.AddComponent<SizeChanger>();
+            ScaleChanger.AddComponent<BoxCollider>();
+            ScaleChanger.GetComponent<BoxCollider>().isTrigger = true;
+            ScaleChanger.tag = "NoCrazyCheck";
+            ScaleChanger.GetComponent<SizeChanger>().minScale = Size.Value;
+            ScaleChanger.GetComponent<SizeChanger>().myType = SizeChanger.ChangerType.Static;
+            ScaleChanger.GetComponent<SizeChanger>().myCollider = ScaleChanger.GetComponent<BoxCollider>();
+            ScaleChanger.GetComponent<Transform>().position = new Vector3(999999, 999999, 999999);
+            ScaleChanger.SetActive(false);
+            var SizeChangeFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "GorillaGoSmall.cfg"), true);
+            Size = SizeChangeFile.Bind("Configuration", "Size", 0.1f, "What size do you want to be?, 0.1 is the normal small scale");
         }
 
         /* This attribute tells Utilla to call this method when a modded room is joined */
@@ -91,7 +93,12 @@ namespace GorillaGoSmallGorillaGoBig
             /* This code will run regardless of if the mod is enabled*/
 
             inRoom = true;
-            SetScale(0.1f, true);
+
+            if(IsEnabled == true)
+            {
+                ScaleChanger.SetActive(true);
+                ScaleChanger.GetComponent<SizeChanger>().minScale = Size.Value;
+            }
         }
 
         /* This attribute tells Utilla to call this method when a modded room is left */
@@ -100,8 +107,10 @@ namespace GorillaGoSmallGorillaGoBig
         {
             /* Deactivate your mod here */
             /* This code will run regardless of if the mod is enabled*/
+
             inRoom = false;
-            SetScale(1, false);
+            ScaleChanger.SetActive(false);
+            ScaleChanger.GetComponent<SizeChanger>().minScale = 1f;
         }
     }
 }
