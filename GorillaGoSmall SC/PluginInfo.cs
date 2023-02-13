8ffaafdb7ccc1b8fafa339c5d5096c2d34f6fac1
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-02-13 17:31:14 -0600

    Update PluginInfo.cs

--- GorillaGoSmall SC/PluginInfo.cs
+++ GorillaGoSmall SC/PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.mrbanana.gorillatag.gorillagosmall";
         public const string Name = "GorillaGoSmall";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.3";
     }
 }
