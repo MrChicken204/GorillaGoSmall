212403bf27e6441d5d639de0816154d5eafcaf3a
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-02-13 18:21:31 -0600

    Update Plugin.cs

--- GorillaGoSmall SC/Plugin.cs
+++ GorillaGoSmall SC/Plugin.cs
@@ -1,110 +1,90 @@
-using BepInEx;
 using System;
 using System.ComponentModel;
-using UnityEngine;
-using Utilla;
-using Player = GorillaLocomotion.Player;
-using BepInEx.Configuration;
 using System.IO;
-using System.Linq;
-using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using GorillaLocomotion;
+using Utilla;
 
 namespace GorillaGoSmallGorillaGoBig
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    // Token: 0x02000003 RID: 3
     [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.7")]
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     [Description("HauntedModMenu")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public static Plugin Instance { get; private set; }
-        public bool IsEnabled;
-        public bool inRoom = false;
-        public static ConfigEntry<float> Size;
-
-        void Start()
+        private void Start()
         {
-            /* A lot of Gorilla Tag systems will not be set up when start is called /*
-			/* Put code in OnGameInitialized to avoid null references */
-
-            Utilla.Events.GameInitialized += OnGameInitialized;
+            Events.GameInitialized += this.OnGameInitialized;
+            SetScale(1f, false);
         }
 
-        public void SetScale(float scale, bool isSmall) //This code was made by Dev(they/them). and is part of the Gorilla's Doom mod
+        public void SetScale(float scale, bool isSmall)
         {
-            Player.Instance.TryGetComponent(out SizeManager sizeManager);
+            Player.Instance.TryGetComponent<SizeManager>(out SizeManager sizeManager);
             if (isSmall)
             {
                 sizeManager.enabled = false;
                 Player.Instance.scale = scale;
             }
-            sizeManager.enabled = true;
+            else
+            {
+                sizeManager.enabled = true;
+            }
         }
 
-        void OnEnable()
+        private void OnEnable()
         {
-            /* Set up your mod here */
-            /* Code here runs at the start and whenever your mod is enabled*/
             HarmonyPatches.ApplyHarmonyPatches();
             IsEnabled = true;
-
-            if(inRoom == true)
+            if (inRoom == true)
             {
                 SetScale(Size.Value, true);
             }
             else
             {
-                SetScale(1, false);
+                SetScale(1f, false);
             }
         }
 
-        void OnDisable()
+        private void OnDisable()
         {
-            /* Undo mod setup here */
-            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
-            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
-
             HarmonyPatches.RemoveHarmonyPatches();
             IsEnabled = false;
-
-            SetScale(1, false);
+            SetScale(1f, false);
         }
 
-        void OnGameInitialized(object sender, EventArgs e)
+        private void OnGameInitialized(object sender, EventArgs e)
         {
+            SetScale(1f, false);
             var SizeChangeFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "GorillaGoSmall.cfg"), true);
             Size = SizeChangeFile.Bind("Configuration", "Size", 0.1f, "What size do you want to be?, 0.1 is the normal small scale");
         }
 
-        /* This attribute tells Utilla to call this method when a modded room is joined */
+        private void Update()
+        {
+        }
+
         [ModdedGamemodeJoin]
         public void OnJoin(string gamemode)
         {
-            /* Activate your mod here */
-            /* This code will run regardless of if the mod is enabled*/
-
             inRoom = true;
-
-            if(IsEnabled == true)
-            {
-                SetScale(Size.Value, true);
-            }
+            SetScale(Size.Value, true);
         }
 
-        /* This attribute tells Utilla to call this method when a modded room is left */
         [ModdedGamemodeLeave]
         public void OnLeave(string gamemode)
         {
-            /* Deactivate your mod here */
-            /* This code will run regardless of if the mod is enabled*/
-
             inRoom = false;
-            SetScale(1, false);
+            SetScale(1f, false);
         }
+
+        public bool IsEnabled;
+
+        public bool inRoom = false;
+
+        public static ConfigEntry<float> Size;
     }
 }
