5b5140ca1dd222bbebb49e650f25c1545ac97105
MrBanana <72209985+pixel773@users.noreply.github.com>
2023-03-25 13:01:31 -0500

    Update Plugin.cs

--- GorillaGoSmall SC/Plugin.cs
+++ GorillaGoSmall SC/Plugin.cs
@@ -3,36 +3,36 @@ using System.ComponentModel;
 using System.IO;
 using BepInEx;
 using BepInEx.Configuration;
-using GorillaLocomotion;
 using Utilla;
+using GorillaGoSmallGorillaGoBig.Scripts;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR;
 
 namespace GorillaGoSmallGorillaGoBig
 {
-    // Token: 0x02000003 RID: 3
     [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.7")]
     [Description("HauntedModMenu")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Start()
+        private GameObject SizeChanger;
+        public ScaleChange ScaleChange { get; private set; }
+        public static Plugin Instance { get; private set; }
+
+        //Button
+        private bool BButton;
+        private bool AButton;
+        private GameObject Player;
+
+        internal void Start()
         {
             Events.GameInitialized += this.OnGameInitialized;
-            SetScale(1f, false);
-        }
-
-        public void SetScale(float scale, bool isSmall)
-        {
-            Player.Instance.TryGetComponent<SizeManager>(out SizeManager sizeManager);
-            if (isSmall)
-            {
-                sizeManager.enabled = false;
-                Player.Instance.scale = scale;
-            }
-            else
-            {
-                sizeManager.enabled = true;
-            }
+            //ScaleChange = new ScaleChange();
+            //ScaleChange.SetScale(1f, false);
+            Instance = this;
+            Events.GameInitialized += OnGameInitialized;
         }
 
         private void OnEnable()
@@ -41,44 +41,86 @@ namespace GorillaGoSmallGorillaGoBig
             IsEnabled = true;
             if (inRoom == true)
             {
-                SetScale(Size.Value, true);
+                SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(9999, 9999, 9999);
+                Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 2;
+                SizeChanger.GetComponent<SizeChanger>().minScale = Size.Value;
+                //ScaleChange.SetScale(Size.Value, true);
             }
             else
             {
-                SetScale(1f, false);
+                SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(17.906f, 1.6123f, 18.0854f);
+                Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 1;
+                SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
+                //ScaleChange.SetScale(1f, false);
             }
         }
 
         private void OnDisable()
         {
+            SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(17.906f, 1.6123f, 18.0854f);
+            SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
             HarmonyPatches.RemoveHarmonyPatches();
             IsEnabled = false;
-            SetScale(1f, false);
+            //ScaleChange.SetScale(1f, false);
         }
 
         private void OnGameInitialized(object sender, EventArgs e)
         {
-            SetScale(1f, false);
+            SizeChanger = GameObject.Find("OuterTinySizer (1)");
+            SizeChanger.GetComponent<Transform>().parent = null;
+            Player = GameObject.Find("GorillaPlayer");
+            //ScaleChange.SetScale(1f, false);
             var SizeChangeFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "GorillaGoSmall.cfg"), true);
             Size = SizeChangeFile.Bind("Configuration", "Size", 0.1f, "What size do you want to be?, 0.1 is the normal small scale");
+            inRoom = false;
+            SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(17.906f, 1.6123f, 18.0854f);
+            Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 1;
+            SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
         }
 
         private void Update()
         {
+            List<InputDevice> list = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+            list[0].TryGetFeatureValue(CommonUsages.secondaryButton, out BButton);
+            list[0].TryGetFeatureValue(CommonUsages.primaryButton, out AButton);
+
+            if(AButton == true && BButton == false /*/&& inRoom == true/*/)
+            {
+                SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(9999, 9999, 9999);
+                Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 1;
+                SizeChanger.GetComponent<SizeChanger>().minScale = 0.1f;
+            }
+
+            if (AButton == false && BButton == true /*/&& inRoom == true/*/)
+            {
+                SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(9999, 9999, 9999);
+                Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 2;
+                SizeChanger.GetComponent<SizeChanger>().minScale = Size.Value;
+            }
         }
 
         [ModdedGamemodeJoin]
         public void OnJoin(string gamemode)
         {
-            inRoom = true;
-            SetScale(Size.Value, true);
+            if(IsEnabled == true)
+            {
+                //ScaleChange.SetScale(Size.Value, true);
+                SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(9999, 9999, 9999);
+                Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 2;
+                SizeChanger.GetComponent<SizeChanger>().minScale = Size.Value;
+                inRoom = true;
+            }
         }
 
         [ModdedGamemodeLeave]
         public void OnLeave(string gamemode)
         {
             inRoom = false;
-            SetScale(1f, false);
+            SizeChanger.GetComponent<Transform>().transform.localScale = new Vector3(17.906f, 1.6123f, 18.0854f);
+            Player.GetComponent<SizeManager>().currentSizeLayerMaskValue = 1;
+            SizeChanger.GetComponent<SizeChanger>().minScale = 0.03f;
+            //ScaleChange.SetScale(1f, false);
         }
 
         public bool IsEnabled;
